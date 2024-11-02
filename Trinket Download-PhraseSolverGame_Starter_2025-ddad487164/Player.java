2b9ce47d90c0d14828757ccd70240ecd392eff1c
haasinimeesala <meesala1032@mydusd.org>
2024-11-02 14:27:07 -0700

    Update Player.java

--- Trinket Download-PhraseSolverGame_Starter_2025-ddad487164/Player.java
+++ Trinket Download-PhraseSolverGame_Starter_2025-ddad487164/Player.java
@@ -1,17 +1,44 @@
-/*
- * Activity 2.5.2
- * 
- * A Player class the PhraseSolverGame
- */
 import java.util.Scanner;
 
-public class Player
-{
-  /* your code here - attributes */
+public class Player {
+    public String name;
+    public int points;
 
-  /* your code here - constructor(s) */ 
+    // Default constructor that prompts the user to enter their name
+    public Player() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter player's name: ");
+        this.name = scanner.nextLine();
+        this.points = 0;
+        System.out.println("Welcome to the game, " + name + "!");
+    }
 
-  /* your code here - accessor(s) */ 
+    // Parameterized constructor that takes inputName and initializes name to inputName
+    public Player(String inputName) {
+        this.name = inputName;
+        this.points = 0;
+        System.out.println("Welcome to the game, " + name + "!");
+    }
 
-  /* your code here - mutator(s) */ 
+    // Copy constructor (optional for testing purposes)
+    public Player(Player p) {
+        this.name = p.name;
+        this.points = p.points;
+        p.name = "Overwritten Name";  // Just for testing the copy effect
+    }
+
+    // Accessor method for name
+    public String getName() {
+        return name;
+    }
+
+    // Accessor method for points
+    public int getPoints() {
+        return points;
+    }
+
+    // Mutator method for points
+    public void addPoints(int additionalPoints) {
+        points += additionalPoints;
+    }
 }
